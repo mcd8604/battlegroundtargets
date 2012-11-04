1ef7233f17beb626e3d633d5bd7e62e738599e81
Sayclub <Sayclub@c89366df-1d54-47ac-aa20-b9730ecf46f4>
2012-11-04 01:52:10 +0000

    koKR update

--- BattlegroundTargets-localization-koKR.lua
+++ BattlegroundTargets-localization-koKR.lua
@@ -19,23 +19,23 @@ L["Summary"] = "요약"
 L["Copy this settings to %s"] = "%s 에 대한 설정을 복사합니다"
 L["Role"] = ROLE
 L["Specialization"] = SPECIALIZATION
-L["Class Icon"] = "직업 아이콘" -- need check - old: ["Show Class Icon"] = "직업 아이콘 표시"
-L["Realm"] = "서버" -- need check - old: ["Hide Realm"] = "서버 숨김"
-L["Leader"] = "전장 지휘관" -- need check - old: ["Show Leader"] = "전장 지휘관 표시"
-L["Guild Groups"] = "길드 그룹" -- need check - old: ["Show Guild Groups"] = "길드 그룹 표시"
+L["Class Icon"] = "직업 아이콘"
+L["Realm"] = "서버"
+L["Leader"] = "전장 지휘관"
+L["Guild Groups"] = "길드 그룹"
 L["Target"] = TARGET
-L["Focus"] = FOCUS
-L["Flag + Orb"] = true
-L["Main Assist Target"] = "지원공격 전담 대상" -- need check - old: ["Show Main Assist Target"] = "지원공격 전담 대상 표시"
-L["Target Count"] = "대상 카운트" -- need check - old: ["Show Target Count"] = "대상 카운트 표시"
-L["Health Bar"] = "생명력 바" -- need check - old: ["Show Health Bar"] = "생명력 바 표시"
-L["Percentage"] = "백분율" -- need check - old: ["Show Percent"] = "백분율 표시"
-L["Range"] = "범위" -- need check - old: ["Show Range"] = "범위 표시"
- L["This option uses the CombatLog to check range."] = true
+L["Focus"] = MINIMAP_TRACKING_FOCUS
+L["Flag + Orb"] = "깃발"
+L["Main Assist Target"] = "지원공격 전담 대상"
+L["Target Count"] = "대상 카운트"
+L["Health Bar"] = "생명력 바"
+L["Percentage"] = "백분율"
+L["Range"] = "범위"
+ L["This option uses the CombatLog to check range."] = "이 옵션은 범위 체크를 위해CombatLog를 사용합니다."
  L["This option uses a pre-defined spell to check range:"] = "이 옵션은 범위 체크를 위해 미리 지정한 주문을 사용합니다:"
- L["Mix"] = true
- L["if you are attacked only"] = true
- L["(class dependent)"] = true
+ L["Mix"] = "혼합"
+ L["if you are attacked only"] = "자신이 공격당할때"
+ L["(class dependent)"] = "(종속 직업)"
  L["Disable this option if you have CPU/FPS problems in combat."] = "전투 상태시 CPU/FPS에 문제가 있다면 이 옵션을 해제하십시오."
 L["Sort By"] = "정렬 방법"
 L["Class"] = CLASS
